2fb84c671556d06c75556dbc74c966ccc3482ddb
kevincwu0 <kevincwu0@gmail.com>
2017-08-21 19:53:08 -0700

    Add dataset and convolution layer of CNN

---
+++ cnn.py
@@ -0,0 +1,27 @@
+# Convolutional Neural Network
+
+# Installing Theano
+# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+
+# Installing Tensorflow
+# pip install tensorflow
+
+# Installing Keras
+# pip install --upgrade keras
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Step 1 - Convolution
+# Convolution - input image, applying feature detectors => feature map
+# 3D Array because colored images
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
